a286c85aafc00f3cee5e5879a9feac20b0c5c088
[Sarah] <[sebetterton@gmail.com]>
2021-11-15 11:05:37 -0500

    Added Json and 'finish' method for a total of 3 methods.

--- BFCalculatorLibrary/BFCalculatorLibrary.cs
+++ BFCalculatorLibrary/BFCalculatorLibrary.cs
@@ -1,23 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Diagnostics;
-using System;
+using Newtonsoft.Json;
 
-namespace BFCalculatorProgram
+namespace BFCalculatorLibrary
 {
     public class Calculator
 
     {
+        JsonWriter writer;
         public Calculator()
         {
-            StreamWriter logFile = File.CreateText("BFcalculator.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting BFCalculator Log");
-            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
         }
         public double DoOperation(double width, double length, double thickness, string WoodType)
         {
             double result = double.NaN; //Default is 'not a number' if an operation could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(width);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(length);
+            writer.WritePropertyName("Operand3");
+            writer.WriteValue(thickness);
+            writer.WritePropertyName("Operation");
 
             //utilizing switch statements for caluclations.
             switch (WoodType)
@@ -29,7 +41,7 @@ namespace BFCalculatorProgram
                             if (thickness != 0)
                             {
                                 result = ((width / 12) * (length / 12) * (thickness) * 10);
-                                Trace.WriteLine(String.Format("({0}/12 * {1}/12 * {2} * 10)", width, length, thickness, result));
+                                writer.WriteValue("Walnut");
                             }
                     break;
                 case "b":
@@ -39,7 +51,7 @@ namespace BFCalculatorProgram
                             if (thickness != 0)
                             {
                                 result = ((width / 12) * (length / 12) * (thickness) * 6.75);
-                                Trace.WriteLine(String.Format("({0}/12 * {1}/12 * {2} * 6.75)", width, length, thickness, result));
+                                writer.WriteValue("Oak");
                             }
                     break;
                 case "c":
@@ -49,7 +61,7 @@ namespace BFCalculatorProgram
                             if (thickness != 0)
                             {
                                 result = ((width / 12) * (length / 12) * (thickness) * 6);
-                                Trace.WriteLine(String.Format("({0}/12 * {1}/12 * {2} * 6)", width, length, thickness, result));
+                                writer.WriteValue("Ash");
                             }
                     break;
                 case "d":
@@ -59,16 +71,26 @@ namespace BFCalculatorProgram
                             if (thickness != 0)
                             {
                                 result = ((width / 12) * (length / 12) * (thickness) * 8);
-                                Trace.WriteLine(String.Format("({0}/12 * {1}/12 * {2} * 8)", width, length, thickness, result));
+                                writer.WriteValue("Maple");
                             }
                     break;
                 // Return text for an incorrect option entry.
                 default:
                     break;
             }
-            return result;
+           
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
 
+            return result;
         }
 
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
     }
 }
